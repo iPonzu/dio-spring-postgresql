9e3c6f874c5176adc25fb76fd815d0c348073e25
iPonzu <joao_ignaciocastro1603@hotmail.com>
2025-05-02 19:44:40 -0300

    single commit

--- src/main/java/diojpa/model/User.java
+++ src/main/java/diojpa/model/User.java
@@ -49,7 +49,7 @@ public class User {
                 "id=" + id +
                 ", name='" + name + '\'' +
                 ", username='" + username + '\'' +
-                ", password='" + password + '\'' 
+                ", password='" + password + '\'' +
                 '}';
                 
     }
